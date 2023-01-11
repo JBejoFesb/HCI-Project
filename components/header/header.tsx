904219cf98e79db749eb1a1e5bf90e8314d940cf
A <abartu00@fesb.hr>
2023-01-11 23:17:12 +0100

    deployment fix and tailwind testing

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,9 +3,8 @@ import styles from "./header.module.css";
 
 const Header = () => {
     return(
-            <div className="">Header Test</div>
+            <div className=" bg-slate-600 text-right">Header Test</div>
     );
-    
 }
 
 export default Header;
