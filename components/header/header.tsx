ca6320140a277e26990fca176f3cc9b9793e2f01
JBejoFesb <jbejo00@fesb.hr>
2023-01-22 15:52:52 +0100

    Added @heroicons by Tailwind to the project for simple svg use

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import style from "./header.module.css";
 import HCLlogo from "../../public/images/icons/HCLtransparent.png";
-import bell from "../../public/images/icons/bell.svg"
-import search from "../../public/images/icons/search.svg"
+import {MagnifyingGlassIcon} from '@heroicons/react/24/outline'
+import {BellAlertIcon} from '@heroicons/react/24/outline'
+import {UserIcon} from '@heroicons/react/24/solid'
 import Link from 'next/link';
 import { useRouter } from "next/router";
 
@@ -16,36 +17,38 @@ const Header = () => {
                 <div className={style.header_left}>
                     <Link href="/"><img className={style.logo} src={HCLlogo.src} alt="HCL logo" /></Link>
                     <ul className={style.nav_links}>
-                        <li className={router.pathname == "/news" ? " text-orange-500 font-bold border-b-2 border-orange-500" : "text-slate-400 "}>
-                            <Link href="/news">Vijesti</Link>
+                        <li className={router.pathname == "/news" ? " text-orange-500 font-bold border-b-2 border-orange-500" : "text-slate-400"}>
+                            <Link className="py-4 px-2" href="/news">Vijesti</Link>
                         </li>
                         <li  className={router.pathname == "/reviews" ? "text-orange-500 font-bold border-b-2 border-orange-500" : "text-slate-400 "}>
-                            <Link href="/reviews">Recenzije</Link>
+                            <Link className="py-4 px-2" href="/reviews">Recenzije</Link>
                         </li>
                         <li  className={router.pathname == "/specials" ? "text-orange-500 font-bold border-b-2 border-orange-500" : "text-slate-400 "}>
-                            <Link href="/specials">Specijali</Link>
+                            <Link className="py-4 px-2" href="/specials">Specijali</Link>
                         </li>
                         <li  className={router.pathname == "/video" ? "text-orange-500 font-bold border-b-2 border-orange-500" : "text-slate-400 "}>
-                            <Link href="/video">Video</Link>
+                            <Link className="py-4 px-2" href="/video">Video</Link>
                         </li>
                         <li  className={router.pathname == "/forum" ? "text-orange-500 font-bold border-b-2 border-orange-500" : "text-slate-400 "}>
-                            <Link href="/">Forum</Link>
+                            <Link className="py-4 px-2" href="/">Forum</Link>
                         </li>
                     </ul>
                 </div>
                 <div className={style.header_right}>
-
-                    <input 
-                        type="text" 
-                        placeholder="Pretraži portal..."
-                        className={style.searchbar}
-                    />
+                    <form className={style.search}>
+                        <input 
+                            type="text" 
+                            placeholder="Pretraži portal..."
+                            className={style.searchbar}
+                        />
+                        <button><MagnifyingGlassIcon className="h-6 w-6 hover:text-orange-500 hover:scale-110"/></button>
+                    </form>
                     <div className={style.user}>
-
+                        <button className="group px-2 py-4"><BellAlertIcon className="h-10 w-10 group-hover:text-orange-500 group-hover:scale-110" /></button>
+                        <button className="group px-2 py-4"><UserIcon className="h-10 w-10 border-white border-2 rounded-full group-hover:text-orange-500 group-hover:scale-110 group-hover:border-orange-500" /></button>
                     </div>
                 </div>
             </div>
-
         </>
     );
 }
