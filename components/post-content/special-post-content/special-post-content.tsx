cc44de01ddc6f6a77dbdd61a354ce4c1338d47d2
JBejoFesb <jbejo00@fesb.hr>
2023-01-26 08:16:14 +0100

    Comments, and dark mode contrast

--- components/post-content/special-post-content/special-post-content.tsx
+++ components/post-content/special-post-content/special-post-content.tsx
@@ -39,6 +39,22 @@ const SpecialPostContent: React.FC<ISpecialPostContainer> = ({ specialPost, vide
                     <h3 className="xl:hidden"><Link href='/impressum' className="text-main-orange hover:underline font-semibold cursor-pointer">{specialPost.author}</Link> | <span><TimeSince creation={specialPost.creation} /> | </span><Link href='/news' className=" text-main-orange cursor-pointer hover:underline font-semibold">{translateTypes[specialPost.type.type]}</Link></h3>
                     <img className=" w-full max-w-none rounded-xl xl:hidden" src={specialPost.mainImage.url} alt={specialPost.title}/>
                     {documentToReactComponents(specialPost.content.json, renderOptions(specialPost.content.links, specialPost.title) as Options)}
+                    <div className="bg-slate-800 h-[500px] rounded-xl">
+                        <div>
+                        <form className="p-5">
+                            <div className="mb-6">
+                                <h2>Komentari:</h2>
+                                <label className="block m-2 text-sm font-medium text-gray-900 dark:text-white">Tvoj email</label>
+                                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required></input>
+                            </div>
+                            <div className="mb-6">
+                                <label className="block m-2 text-sm font-medium text-gray-900 dark:text-white">Komentar</label>
+                                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required></input>
+                            </div>
+                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ostavi komentar</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
                 <SidebarContainer data={[{data: videoPosts, key: 'video'}, {data: popularPosts, key: 'aktualno'}]} widgets={true}/>
             </div>
