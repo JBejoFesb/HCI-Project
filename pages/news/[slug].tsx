85c258f7c7a6720414421ad37d2322dca23eaf20
JBejoFesb <jbejo00@fesb.hr>
2023-01-26 01:30:15 +0100

    Bug fixes for renderer

--- pages/news/[slug].tsx
+++ pages/news/[slug].tsx
@@ -13,7 +13,7 @@ const NewsPost: React.FC<INewsPostContainer> = ({ newsPost }) => {
             <title>{newsPost.title}</title>
         </Head>
         <Header/>
-        <div className="pt-[72px] md:pt-24 lg:pt-28 2xl:pt-32 md:pb-10 flex flex-col items-center bg-gradient-to-b from-slate-900 via-blue-900 to-slate-900">
+        <div className="pt-[72px] md:pt-24 lg:pt-28 2xl:pt-32 md:pb-10 flex flex-col items-center bg-slate-800">
             <div className=' flex flex-col items-center'>
                 <div className=' flex flex-row justify-between gap-5 2xl:gap-10 lg:pl-5 lg:pr-5 2xl:pl-10 2xl:pr-10'>
                     <NewsPostContent newsPost={newsPost} />
