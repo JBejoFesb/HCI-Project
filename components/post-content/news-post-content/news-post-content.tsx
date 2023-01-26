08e67a3b7aefc9f2a4b3137512eea621ebe58ff2
JBejoFesb <jbejo00@fesb.hr>
2023-01-26 03:07:38 +0100

    Mobile article finished

--- components/post-content/news-post-content/news-post-content.tsx
+++ components/post-content/news-post-content/news-post-content.tsx
@@ -4,9 +4,13 @@ import renderOptions from "../render-options";
 import { IType, IImage, translateTypes } from "../../../constants/constants";
 import TimeSince from "../../time-since/time-since";
 import Link from "next/link";
+import SidebarContainer from "../../../components/sidebar/sidebar-container/sidebar-container";
+import { ICurrentNews } from "../../sidebar/parts/current-news/current-news";
 
 export interface INewsPostContainer {
     newsPost: INewsPostContent,
+    videoPosts: ICurrentNews[],
+    popularPosts: ICurrentNews[],
 }
 
 export interface INewsPostContent {
@@ -20,14 +24,22 @@ export interface INewsPostContent {
     content: any,
 }
 
-const NewsPostContent: React.FC<INewsPostContainer> = ({ newsPost }) => {
+const NewsPostContent: React.FC<INewsPostContainer> = ({ newsPost, videoPosts, popularPosts }) => {
     return (
-        <div className=" flex flex-col bg-slate-800 md:rounded-3xl max-w-[800px]">
-            <div className=" flex flex-col pt-5 pr-5 pl-5 pb-5 gap-6 lg:gap-8">
+        <div className=' flex flex-row xl:flex-col gap-5 lg:pl-5 lg:pr-5 2xl:pl-10 2xl:pr-10'>
+            <div className="max-xl:hidden flex flex-col px-2 gap-6 xl:max-w-[1100px]">
                 <h1>{newsPost.title}</h1>
                 <h3><Link href='/impressum' className="text-main-orange hover:underline font-semibold cursor-pointer">{newsPost.author}</Link> | <span><TimeSince creation={newsPost.creation} /> | </span><Link href='/news' className=" text-main-orange cursor-pointer hover:underline font-semibold">{translateTypes[newsPost.type.type]}</Link></h3>
                 <img className=" w-full max-w-none rounded-xl" src={newsPost.mainImage.url} alt={newsPost.title}/>
-                {documentToReactComponents(newsPost.content.json, renderOptions(newsPost.content.links, newsPost.title) as Options)}
+            </div>
+            <div className=" flex flex-col xl:flex-row justify-between xl:max-w-[1100px]">
+                <div className=" flex flex-col p-5 xl:px-2 xl:py-0  gap-6 lg:gap-8 max-w-[750px]">
+                    <h1 className="xl:hidden">{newsPost.title}</h1>
+                    <h3 className="xl:hidden"><Link href='/impressum' className="text-main-orange hover:underline font-semibold cursor-pointer">{newsPost.author}</Link> | <span><TimeSince creation={newsPost.creation} /> | </span><Link href='/news' className=" text-main-orange cursor-pointer hover:underline font-semibold">{translateTypes[newsPost.type.type]}</Link></h3>
+                    <img className=" w-full max-w-none rounded-xl xl:hidden" src={newsPost.mainImage.url} alt={newsPost.title}/>
+                    {documentToReactComponents(newsPost.content.json, renderOptions(newsPost.content.links, newsPost.title) as Options)}
+                </div>
+                <SidebarContainer data={[{data: videoPosts, key: 'video'}, {data: popularPosts, key: 'aktualno'}]} widgets={true}/>
             </div>
         </div>
     );
--- pages/news/[slug].tsx
+++ pages/news/[slug].tsx
@@ -1,24 +1,20 @@
 import React from "react";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
-import { getNewsPostBySlug, getAllNewsSlugs } from "../api/ContentfulAPI";
-import SidebarContainer from "../../components/sidebar/sidebar-container/sidebar-container";
+import { getNewsPostBySlug, getAllNewsSlugs, getNewVideoPosts, getPopularPosts } from "../api/ContentfulAPI";
 import NewsPostContent, { INewsPostContent, INewsPostContainer } from "../../components/post-content/news-post-content/news-post-content";
 import Head from "next/head";
 
-const NewsPost: React.FC<INewsPostContainer> = ({ newsPost }) => {
+const NewsPost: React.FC<INewsPostContainer> = ({ newsPost, videoPosts, popularPosts }) => {
     return (
         <>
         <Head>
             <title>{newsPost.title}</title>
         </Head>
         <Header/>
-        <div className="pt-[72px] md:pt-24 lg:pt-28 2xl:pt-32 md:pb-10 flex flex-col items-center bg-slate-800">
+        <div className="pt-[72px] md:pt-24 md:pb-4 flex flex-col items-center bg-slate-800">
             <div className=' flex flex-col items-center'>
-                <div className=' flex flex-row justify-between gap-5 2xl:gap-10 lg:pl-5 lg:pr-5 2xl:pl-10 2xl:pr-10'>
-                    <NewsPostContent newsPost={newsPost} />
-                    <SidebarContainer data={[]} widgets={true}/>
-                </div>
+                    <NewsPostContent newsPost={newsPost} videoPosts={videoPosts} popularPosts={popularPosts} />
             </div>
         </div>
         <Footer />
@@ -44,8 +40,10 @@ export async function getStaticPaths() {
 export async function getStaticProps(context: any) {
     const { slug } = context.params;
     const newsPost = await getNewsPostBySlug(slug);
+    const videoPosts = await getNewVideoPosts();
+    const popularPosts = await getPopularPosts();
 
     return {
-        props: { newsPost: newsPost }
+        props: { newsPost: newsPost, videoPosts: videoPosts, popularPosts: popularPosts }
     }
 }
