bc4b217aa640e3c1e06735a6cc61e125b6cf80de
A <abartu00@fesb.hr>
2023-02-21 16:02:01 +0100

    userbase rm react package and setup appId

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { useState, useEffect } from 'react';
+import userbase from 'userbase-js';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState();
+  useEffect(() => {
+    userbase.init({appId: process.env.REACT_APP_USERBASE_APP_ID as string})
+  }, []);
+
+  return <Component user={user} {...pageProps} />
 }
