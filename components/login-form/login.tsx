62106374b4df6b8ac72096affd781fc104061129
A <abartu00@fesb.hr>
2023-02-25 19:46:54 +0100

    test commit

--- components/login-form/login.tsx
+++ components/login-form/login.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
+import { SHA256 } from "crypto-js";
 
 interface LoginProps {
     onClose: () => void;
@@ -7,21 +9,28 @@ interface LoginProps {
 
 const Login: React.FC<LoginProps> = ({ onClose }) => {
 
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    function handleSubmit(){
-        // ode poslat backendu
+    const handleSubmit = () => {
+        event?.preventDefault();
+        console.log("TEST");
+        axios
+        .get(
+            `https://ec2-3-70-46-221.eu-central-1.compute.amazonaws.com/comments/load`
+        ).then( response => {
+            console.log(response)
+        }).catch(error => console.log(error))
         // kad uspjesno zavrsi poslat onClose opet
-    };
+    }
 
     return(
         <div className="bg-black/30 absolute h-full w-full flex justify-center items-center z-50">
             <div className="bg-slate-900 flex h-fit rounded-xl p-4">
                 <form onSubmit={handleSubmit} className="flex flex-col">
                     <label>
-                        Email:
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        Username:
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
                     </label>
                     <label>
                         Password:
