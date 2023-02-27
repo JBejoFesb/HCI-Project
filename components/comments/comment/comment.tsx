bc72b1d962707a5d2ffaf7ec1a719636c2392864
JBejoFesb <jbejo00@fesb.hr>
2023-02-27 02:13:24 +0100

    Final update to comments

--- components/comments/comment/comment.tsx
+++ components/comments/comment/comment.tsx
@@ -5,7 +5,7 @@ export interface IComment {
 
 const Comment: React.FC<IComment> = ({ username, content }) => {
     return (
-        <div className=" flex flex-col py-4 px-2 mb-3 bg-black">
+        <div className=" flex flex-col py-4 px-2 mb-3 gap-2 rounded-md bg-slate-700/50">
             <h3>{username}</h3>
             <div>{content}</div>
         </div>
